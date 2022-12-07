38d0f8d249810fecc39556fab01203c7523a8fae
Marqnh0 <119981378+Marqnh0@users.noreply.github.com>
2022-12-06 21:17:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+<script>
+
+let email = document.querySelector("#email")
+let senha = document.querySelector("#senha")
+let btn = document.querySelector("button")
+
+btn.addEventListener("click", entrar)
+
+function entrar(){
+if(email.value && senha.value){
+ alert("EU TE AMO PRA SEMPRE")
+} else {
+ alert("VC MORA NO MEU CORAÇÃO TE AMO")
+}
+
+}
+
+
+
+
+
+</script>
